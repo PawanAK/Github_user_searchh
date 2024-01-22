3df653ebc5fc61a93756189568ebab79036aae96
PawanAK <pawanajjark@gmail.com>
2024-01-22 18:30:13 +0530

    Fix GitHub username in searchGithubUser function

--- src/context/context.js
+++ src/context/context.js
@@ -73,7 +73,7 @@ const GithubProvider = ({ children }) => {
   useEffect(checkRequests, []);
   // get initial user
   useEffect(() => {
-    searchGithubUser('Pawan AK');
+    searchGithubUser('PawanAK');
   }, []);
   return (
     <GithubContext.Provider
