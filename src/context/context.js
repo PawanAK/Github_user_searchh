1d23988be03c9f9349795769953cbd4460a823b7
PawanAK <pawanajjark@gmail.com>
2024-01-22 18:28:33 +0530

    Update Twitter username in Card component and change initial user in GithubProvider

--- src/context/context.js
+++ src/context/context.js
@@ -73,7 +73,7 @@ const GithubProvider = ({ children }) => {
   useEffect(checkRequests, []);
   // get initial user
   useEffect(() => {
-    searchGithubUser('john-smilga');
+    searchGithubUser('Pawan AK');
   }, []);
   return (
     <GithubContext.Provider
